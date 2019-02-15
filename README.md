493dfc68e759ddac8f2cc83de437ef93325724ca
Jonas Genannt <jonas@brachium-system.net>
2019-02-15 23:11:36 +0100

    pluse only no blue eye

--- README.md
+++ README.md
@@ -2,5 +2,4 @@
 
  * ATTiny85
  * RGB LED PL9823
- * 2x 3mm LED blue for the eyes
- * one switch with copper tape to change pulse color
+ * one push switch with copper tape to change pulse color
--- src/main.cpp
+++ src/main.cpp
@@ -1,77 +1,60 @@
 #include <Arduino.h>
 #include <FastLED.h>
+#include <Bounce2.h>
 
 const static CRGB::HTMLColorCode colors[35] = {
   CRGB::Crimson,
   CRGB::Red,
-  CRGB::DarkRed,
   CRGB::DeepPink,
   CRGB::OrangeRed,
+  CRGB::SkyBlue,
   CRGB::Orange,
   CRGB::Gold,
   CRGB::Yellow,
+  CRGB::DarkRed,
   CRGB::Violet,
   CRGB::Navy,
-  CRGB::RosyBrown,
   CRGB::Fuchsia,
+  CRGB::RosyBrown,
   CRGB::MediumOrchid,
   CRGB::BlueViolet,
-  CRGB::Purple,
   CRGB::Indigo,
   CRGB::DarkSlateBlue,
   CRGB::GreenYellow,
+  CRGB::Purple,
   CRGB::Lime,
   CRGB::MediumSpringGreen,
   CRGB::ForestGreen,
+  CRGB::CadetBlue,
   CRGB::DarkGreen,
   CRGB::LightSeaGreen,
   CRGB::Teal,
   CRGB::Cyan,
-  CRGB::CadetBlue,
   CRGB::SteelBlue,
-  CRGB::SkyBlue,
   CRGB::Blue,
+  CRGB::Brown,
   CRGB::DarkBlue,
   CRGB::Chocolate,
   CRGB::SaddleBrown,
   CRGB::Maroon,
-  CRGB::DarkSlateGray,
-  CRGB::Brown
+  CRGB::DarkSlateGray
 };
 
 
 CRGB leds[1];
+Bounce debouncer = Bounce();
 #define DIRECTION_UP 1
 #define DIRECTION_DOWN 0
-#define PIN_RGB_LED 3
-#define PIN_EYE_LEFT 4
-#define PIN_EYE_RIGHT 2
-#define PIN_BUTTON 1
+#define PIN_RGB_LED 0
+#define PIN_BUTTON 2
 
 volatile uint8_t brightness = 0;
 volatile uint8_t hold = 0;
 volatile short direction = DIRECTION_UP;
-
-volatile int runs = 0;
-volatile uint8_t eye_left = 1;
-volatile uint8_t sleep_0 = 10;
-volatile uint8_t sleep_1 = 30;
+volatile uint8_t color_index = 0;
 
 // called every ~ 35ms
 ISR(TIMER1_COMPA_vect) {
-	runs++;
-
-	if (runs == 8) {
-		eye_left = 0;
-		sleep_0 = random(10, 12);
-	}
-	if (runs == sleep_0) {
-		eye_left = 1;
-		sleep_1 = random(30, 60);
-	}
-	if (runs > sleep_1) {
-		runs = 0;
-	}
 	if (brightness < 5 && hold < 6) {
 		hold++;
 		return;
@@ -105,30 +88,41 @@ ISR(TIMER1_COMPA_vect) {
 
 void setup() {
 	FastLED.addLeds<PL9823, PIN_RGB_LED>(leds, 1);
+	leds[0] = CRGB::Blue;
+	for (short i = 255; i > 0; i--) {
+		FastLED.setBrightness(i);
+		FastLED.show();
+		delay(10);
+	}
+	delay(15);
 	FastLED.setBrightness(0);
 	FastLED.clear(true);
 	FastLED.show();
 
-	leds[0] = colors[random(34)];
+	leds[0] = CRGB::Red;
 	TCCR1 |= (1 << CTC1);
 	TCCR1 |= (1 << CS13) | (1 << CS11) | (1 << CS10);
 	OCR1C = 280;
 	TIMSK |= (1 << OCIE1A);
 
-	pinMode(PIN_EYE_LEFT, OUTPUT);
-	digitalWrite(PIN_EYE_LEFT, HIGH);
-
-	pinMode(PIN_EYE_RIGHT, OUTPUT);
-	digitalWrite(PIN_EYE_RIGHT, HIGH);
-
 	pinMode(PIN_BUTTON, INPUT_PULLUP);
+
+	debouncer.attach(PIN_BUTTON);
+	debouncer.interval(20);
 }
 
 
 
 void loop() {
+	debouncer.update();
 	FastLED.setBrightness(brightness);
 	FastLED.show();
-	digitalWrite(PIN_EYE_LEFT, eye_left);
-	digitalWrite(PIN_EYE_RIGHT, eye_left);
+
+	if (debouncer.fell()) {
+		color_index += 1;
+		if (color_index > 34) {
+			color_index = 0;
+		}
+		leds[0] = colors[color_index];
+	}
 }
